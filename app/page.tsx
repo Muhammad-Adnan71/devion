4125754655f5829df3f4562ce0203c182b97051a
Muhammad Adnan <58784149+Muhammad-Adnan71@users.noreply.github.com>
2024-01-23 15:36:34 +0500

    Update page.tsx
    remove hero

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import VideoSection from "@components/layout/videoSection";
 export default function Home() {
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
       <VideoSection />
       <AvailabilitySection />
       <ValuesSection />
